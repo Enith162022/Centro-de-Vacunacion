8aafc056483b84785273b801bba616a3e1a1405b
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-11-27 21:59:55 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -14,10 +14,13 @@ import numpy as np
 st.subheader("Integrantes")
 st.write(""" - Aguilar Rojas, Enith""")  
 st.write(""" - Lizarbe Mirko""")
-st.write(""" - Rojas Rua, Rocio holiwi""")
+st.write(""" - Rojas Rua, Rocio""")
 
 st.subheader("¿Cual es el objetivo:") 
 st.write("""Facilitar al usuario la disponibilidad de centros de vacunación, dada por una estrategia para poder promocionar y facilitar la Vacunación en el país""") 
+
+st.subheader("Contexto") 
+st.write("""holi""") 
 
 st.subheader("¿Cuáles son los síntomas del Coronavirus?") 
 ##############################################################################
