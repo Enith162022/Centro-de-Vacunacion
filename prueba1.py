983e8c9cd7cd90ee9a9a7eca9c9402ceec85f838
Mirkolizarbe123 <118316526+Mirkolizarbe123@users.noreply.github.com>
2022-12-02 23:47:47 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -27,7 +27,7 @@ def centros_vac():
             })
     return df_centrosvac
 data = centros_vac()
-        st.map(data)   
+st.map(data)   
 ###################################################
 st.sidebar.header("Barra de menú")
 
