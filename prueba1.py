955b1dc1e0cefd09c46f3b7d75474e75365aa67a
Mirkolizarbe123 <118316526+Mirkolizarbe123@users.noreply.github.com>
2022-12-02 23:35:44 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -17,7 +17,7 @@ def download_data():
     output = "data.csv"
     gdown.download(url,output,quiet = False)
 download_data()
-df = pd.read_csv("data.csv")
+df = pd.read_csv("output")
 
 ###################################################
 st.sidebar.header("Barra de menú")
