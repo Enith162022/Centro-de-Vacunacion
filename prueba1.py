7446be6b72a4b352c2ee4bdf4e4dffff4c7e9687
Mirkolizarbe123 <118316526+Mirkolizarbe123@users.noreply.github.com>
2022-12-02 23:28:25 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -16,9 +16,10 @@ def download_data():
     url = "https://drive.google.com/uc?id= 1Y9A1gXBN8OgLXoNVAOE3Wm7zfCkpf9Y8"
     output = "data.csv"
     gdown.download(url,output,quiet = False)
+    return df
 download_data()
 
-df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=30000)
+st.dataframe(df.head(20))
 ###################################################
 st.sidebar.header("Barra de menú")
 #st.header("sidebar")
