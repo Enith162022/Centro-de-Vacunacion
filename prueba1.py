819a09cdc5ea4ca87b0e36329a767be2eb62e0d4
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-12-09 13:29:48 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -69,7 +69,7 @@ df = pd.read_excel(archivo_excel,
 option = ["Departamentos", "nombre"]
 model = st.sidebar.selectbox("Elija una opción",option)
 
-df_agrupado = df[mask].groupby(["Departamento"]).count(),[["# Centros de Vacunación"]]
+df_agrupado = df[mask].groupby(["Departamento"]).count()[["# Centros de Vacunación"]]
 #df_agrupado = df_agrupado.rename(columns={# Centros de Vacunación":"Cantidad"})
 #df_agrupado = df_agrupado.reset_index()
 #para grafica de barras
