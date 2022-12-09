b1b0af332b44b9e16509e6afb16324754d2cc6c4
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-12-09 17:04:50 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -73,7 +73,7 @@ st.dataframe(df)
 """### gif from url"""
 st.markdown("![Alt Text](https://lottiefiles.com/17902-covid19)")
 
-file_ = open("https://github.com/Enith162022/Centro-de-Vacunacion/blob/main/17902-covid19.gif", "rb")
+file_ = open("C:\Spyder", "rb")
 contents = file_.read()
 data_url = base64.b64encode(contents).decode("utf-8")
 file_.close()
