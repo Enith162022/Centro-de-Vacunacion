6a46d46e1fc9d5263ed0bf8da6350e88e3162934
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-12-09 14:26:44 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -64,7 +64,7 @@ archivo_excel = 'DATOSF.xlsx'
 hoja_excel = 'TABLA1'
 df = pd.read_excel(archivo_excel,
                    sheet_name = hoja_excel,
-                   usecols = 'A:C')
+                   usecols = 'A:F')
 
 option = ["Departamentos", "nombre"]
 model = st.sidebar.selectbox("Elija una opción",option)
