b4d58ec295cb38b6f43a0a9a0909cba71e67cce4
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-12-08 21:01:07 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -71,7 +71,7 @@ def dowload_data():
 	url="https://drive.google.com/uc?id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM"
 	output="data.csv"
 	gdown.download(url,output,quiet=False)
-download_data()
+def download_data():
 #df = pd.read_csv(r'C:\Users\51952\Downloads\PositivosCovid\positivos_covid.csv',sep=";", skip_blank_lines=True, parse_dates=['id_centro_vacunacion', 'id_eess'])
 df = pd.read_csv('data.csv',sep = ";",  skip_blank_lines=True, nrows=1000,parse_dates=['id_centro_vacunacion', 'id_eess'])
 st.dataframe(data.head(20))
