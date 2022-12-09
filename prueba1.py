8fd63a47dcca14f9b3a4aeb62542fc637912f541
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-12-09 16:44:42 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -69,7 +69,14 @@ df = pd.read_excel(archivo_excel,
                   )
 st.dataframe(df) 
 ################################################################
+file_ = open("https://lottiefiles.com/17902-covid19", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
 
+st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
 
 
 ###########################
