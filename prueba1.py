0bd2e18ce440d0755371f4bee7b8dea61fab03b7
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-12-08 21:05:01 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -73,7 +73,8 @@ def dowload_data():
 
 def download_data():
 #df = pd.read_csv(r'C:\Users\51952\Downloads\PositivosCovid\positivos_covid.csv',sep=";", skip_blank_lines=True, parse_dates=['id_centro_vacunacion', 'id_eess'])
-df = pd.read_csv('data.csv',sep = ";",
+df = pd.read_csv('data.csv',
+                 sep = ";",
                  skip_blank_lines=True, 
                  nrows=1000,
                  parse_dates=['id_centro_vacunacion', 'id_eess'])
