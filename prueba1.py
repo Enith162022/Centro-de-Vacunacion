20ffccd662ffdba7738654e12c66ee6716c4f080
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-12-09 14:39:57 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -60,12 +60,13 @@ with col3:
     
 st.write("**Fuente:** ONU https://www.who.int/es/health-topics/coronavirus#tab=tab_3")
 ################################################################
-archivo_excel = 'DATOSF.xlsx' 
-hoja_excel = 'TABLA1'
+archivo_excel = "DATOSF.xlsx"
+hoja_excel = "TABLA1"
 
 df = pd.read_excel(archivo_excel,
                    sheet_name = hoja_excel,
-                   usecols = 'A:C')
+                   usecols = "A:C")
+
 option = ["Departamentos", "nombre"]
 model = st.sidebar.selectbox("Elija una opción",option)
 
