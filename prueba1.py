4fa1a27357143aa4934cbbe8e88b3d050674fb8d
Enith162022 <117868368+Enith162022@users.noreply.github.com>
2022-12-09 15:42:50 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -69,15 +69,13 @@ df = pd.read_excel(archivo_excel,
                   )
 st.dataframe(df) 
 ################################################################
-df = px.bar(
+dc = px.bar(
     x="Departamento",
     y='C.Vac',
     title = '<b>Departamentos vs C.Vacunación</b>',
     template = 'plotly_white',
 )
-
-st.dataframe(df)
-
+st.bar_chart(dc)
 ###########################
 #id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM
 #@st.experimental_nemo
