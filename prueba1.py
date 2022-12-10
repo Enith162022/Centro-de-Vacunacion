064b61c32542ed22df13af9a1344088a8356ece6
Mirkolizarbe123 <118316526+Mirkolizarbe123@users.noreply.github.com>
2022-12-10 18:07:06 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -339,6 +339,7 @@ if result:
                    usecols = "A:B",
                   )
     st.dataframe(df)
+    
 ##########################################################################################
 #Importar el mapa de localización de los centros de vacunación
 archivo_excel = "coordenadas_de_centros_vac.xlsx"
@@ -349,17 +350,16 @@ df = pd.read_excel(archivo_excel,
 st.dataframe(df)
 
 @st.cache
-def cvac():
-    df_cvac = pd.read_excel(archivo_excel,
-                   sheet_name = hoja_excel,
-                   usecols = "A:C", )
-    df_cvac = df_vac.rename(columns={
-         'latitud':'lat',
-         'longitud':'lon',
-    })
-    return df_cvac
-datos = cvac()
-st.map(datos)
+    def cvac():
+        df_cvac = pd.read_excel(archivo_excel,
+                       sheet_name = hoja_excel,
+                       usecols = "A:C", )
+       df_cvac = df_vac.rename(columns={
+             'latitud':'lat',
+             'longitud':'lon',
+        })
+       return df_cvac
+st.map(cvac())
 
 ##########################################################################################
 #id=13iNig4VIvt5Gm0znUt2eq3_YnGCgCQHM
