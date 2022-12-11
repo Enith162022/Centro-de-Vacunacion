37f1d74154e22a98b9050f83a08372c846067fa7
Mirkolizarbe123 <118316526+Mirkolizarbe123@users.noreply.github.com>
2022-12-10 19:34:51 -0500

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -338,6 +338,14 @@ if result:
                   )
     st.dataframe(df)
 #####################################################33
+#Importar el mapa de localización de los centros de vacunación
+archivo_excel = "coordenadas_de_centros_vac.xlsx"
+hoja_excel = "coordenadas"
+df = pd.read_excel(archivo_excel,
+                   sheet_name = hoja_excel,
+                   usecols = "A:C", )
+st.dataframe(df)
+
 @st.cache
 def cvac():
     df_cvac = pd.read_excel(archivo_excel,
